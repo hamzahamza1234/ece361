6ea63dcac7a78f94dafe990646ac195febfa06de
Caitlin Ludwig <caitlin.ludwig@mail.utoronto.ca>
2024-02-03 13:18:36 -0500

    [Lab 1 Part 2] Added packet struct to server.c  - Made some formatting updates to server.c (ex. changing the lines of curly brackets)

--- Part_2/server.c
+++ Part_2/server.c
@@ -10,10 +10,18 @@
 
 #define BACKLOG 10
 
-int main(int argc, char *argv[])
+struct packet
 {
+    unsigned int total_frag;
+    unsigned int frag_no;
+    unsigned int size;
+    char *filename;
+    char filedata[1000];
+};
+
+int main(int argc, char *argv[]) {
     struct addrinfo hints, *res;
-    int sockfd,new_fd;
+    int sockfd, new_fd;
     struct sockaddr_storage from_addr;
     socklen_t addr_size;
 
@@ -28,60 +36,55 @@ int main(int argc, char *argv[])
 
     // make a socket
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sockfd < 0){
+    if (sockfd < 0) {
         perror("failed to create socket");
         exit(EXIT_FAILURE);
     }
 
 
     //bind it to the port we passed in to getaddrinfo():
-    if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0)
-    {
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
         perror("failed to bind");
         exit(EXIT_FAILURE);
     }
 
-while(1){
-    /// UDP is connectionless so no listening or accepting. just recvfrom to listen and sento to send
-    int length = sizeof(struct sockaddr_storage);
-    int num_bytes = recvfrom(sockfd, (char *)buffer, 4096, 0, (struct sockaddr *) &from_addr, &length); //here from addr will store the address of client
+    while(1) {
+        /// UDP is connectionless so no listening or accepting. just recvfrom to listen and sento to send
+        int length = sizeof(struct sockaddr_storage);
+        int num_bytes = recvfrom(sockfd, (char *)buffer, 4096, 0, (struct sockaddr *) &from_addr, &length); //here from addr will store the address of client
 
-    if (num_bytes < 0)
-    {
-        perror("failed to get message , 0 bytes recieved ");
-        exit(EXIT_FAILURE);
-    }
+        if (num_bytes < 0) {
+            perror("failed to get message , 0 bytes recieved ");
+            exit(EXIT_FAILURE);
+        }
 
-    buffer[length] = '\0';
+        buffer[length] = '\0';
 
-    //our reply if based on what was sent from the client , if ftp was sent , we send a yes, else we send a no
+        //our reply if based on what was sent from the client , if ftp was sent , we send a yes, else we send a no
 
-    char reply_yes[100] = "yes";
-    char reply_no[100] = "no";
-    int sent_bytes;
+        char reply_yes[100] = "yes";
+        char reply_no[100] = "no";
+        int sent_bytes;
 
-    printf("Message from Client: %s\n" , buffer);
+        printf("Message from Client: %s\n" , buffer);
 
-    char check[4096] = "ftp";
+        char check[4096] = "ftp";
 
-    if (strcmp(check, buffer) == 0){
-         printf("Sending yes to client\n");
-        if ((sent_bytes = sendto(sockfd, reply_yes, strlen(reply_yes), 0, (const struct sockaddr *)&from_addr, length)) == -1)
-        {
-            perror("talker: sendto");
-            exit(1);
+        if (strcmp(check, buffer) == 0) {
+            printf("Sending yes to client\n");
+            if ((sent_bytes = sendto(sockfd, reply_yes, strlen(reply_yes), 0, (const struct sockaddr *)&from_addr, length)) == -1) {
+                perror("talker: sendto");
+                exit(1);
+            }
+        }
+        else {
+            printf("Sending no to client\n");
+            if ((sent_bytes = sendto(sockfd, reply_no, strlen(reply_no), 0, (const struct sockaddr *)&from_addr, length)) == -1) {
+                perror("talker: sendto");
+                exit(1);
+            }
         }
     }
-    else{
-        printf("Sending no to client\n");
-        if ((sent_bytes = sendto(sockfd, reply_no, strlen(reply_no), 0, (const struct sockaddr *)&from_addr, length)) == -1)
-        {
-            perror("talker: sendto");
-            exit(1);
-        }
-         
-    }
-}
 
 
     //close the socket in the end
