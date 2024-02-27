6ceb9a0444f1480193fa3b07e8261abcceb37cdf
Hamza Mohammed <ham.mohammed@mail.utoronto.ca>
2024-02-27 17:28:00 -0500

    started adding actual client functionality in terms of the commands, i exit in the middle of the main function right now just for easy debugging

--- Lab_2/client.c
+++ Lab_2/client.c
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <math.h>
+#include <ctype.h>
 
 #define _OPEN_SYS_SOCK_IPV6
 #define MAXDATASIZE 100
@@ -23,11 +24,91 @@ int main(int argc, char *argv[])
     int numbytes;
     char s[INET_ADDRSTRLEN];
 
-    if (argc != 3)
-    { // just to check if both the IP address and port number were given as arguments
-        fprintf(stderr, "usage: talker hostname message\n");
-        exit(1);
-    }
+    // variales needed for login
+    char client_name[100];
+    char client_pass[100];
+    char server_address[100];
+    char server_p[100];
+    int server_port;
+
+
+    while(1){
+        char command[4096];
+        char login_com[4096];
+
+        printf("New Client has started, Please enter a command\n");
+        fgets(command, MAXDATASIZE, stdin);
+
+        //check for /quit or /login first because only those commands can be done first
+        char login_check[4096];
+        strncpy(login_check, command, 6);
+
+        while (strcmp(login_check, "/login") != 0 && strcmp(command, "/quit\n") != 0)
+        {
+            printf("Please Try Again, Enter the login or quit command with correct Arguments\n");
+            fgets(command, MAXDATASIZE, stdin);
+            strncpy(login_check, command, 6);
+        }
+        
+
+        if (strcmp(command,"/quit\n") == 0){
+            break;
+        }
+        else {
+            char delim[]=" ";
+            int num_args = 0;
+            strncpy(login_com, command+7,MAXDATASIZE );
+
+            char* login_ptr = strtok(login_com,delim);
+
+            while(login_ptr != NULL){
+                if (num_args == 0){
+                   strncpy(client_name, login_ptr,100);
+                }
+                else if (num_args == 1)
+                {
+                    strncpy(client_pass, login_ptr, 100);
+                }
+                else if (num_args == 2)
+                {
+                    strncpy(server_address, login_ptr, 100);
+                }
+                else if (num_args == 3)
+                {
+                    strncpy(server_p, login_ptr, 100);
+                }
+
+                num_args++;
+                login_ptr = strtok(NULL, delim);
+            }
+
+            if (num_args != 4){
+                printf("Wrong number of Arguments\n");
+                continue;
+            }
+
+        }
+        int num;
+        char ch;
+
+        if ((sscanf(server_p, "%i%c", &num, &ch) == 0))
+        {
+            printf("port number has to be a NUMBER\n");
+            continue;
+        }
+
+       server_port = atoi(server_p);
+
+
+       printf("client name: %s\n", client_name);
+       printf("client pass: %s\n", client_pass);
+       printf("client ip: %s\n", server_address);
+       printf("server port: %d\n", server_port);
+
+       return 0;
+
+        
+      
 
     memset(&hints, 0, sizeof(hints));
     // filling up some information about the server
@@ -98,5 +179,6 @@ int main(int argc, char *argv[])
 
     freeaddrinfo(servinfo);
     close(sockfd);
+    }
 
 }
